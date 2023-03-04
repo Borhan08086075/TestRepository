9b10410962c4431abc39058a513a76b1eb124e6f
Borhan08086075 <borhan6075@gmail.com>
2023-03-04 11:49:24 -0500

    Added anew class and made some changes to other class

---
+++ JavaFeb23/src/actionitems/AddingNumbers.java
@@ -0,0 +1,29 @@
+package actionitems;
+
+public class AddingNumbers {
+
+	public static void main(String[] args) {
+	
+		
+		/*int sum = 0;
+		for(int i = 0; i<=15; i++)
+		{
+			sum = sum + i;
+		}
+		
+		 System.out.println("The resurlt is :" + sum);*/
+		
+		
+		int sum1 = 0;
+		int i = 0;
+		while(i<=15) {
+			
+			sum1 = sum1 + i ;
+			i++;
+		}
+		System.out.println(sum1);
+		
+
+	}
+
+}
---
+++ JavaFeb23/src/dataTypes/SwitchPractic2.java
@@ -0,0 +1,36 @@
+package dataTypes;
+
+public class SwitchPractic2 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		//sdklfjdslkfjdlkfjdflkdjfd
+	int day=4;	
+	
+	switch(day)
+	
+	
+	{
+	case 1:System.out.println("today is mondsy");break;
+	case 2:System.out.println("today is tuesday");break;
+	case 3:System.out.println("today is wednesday");break;
+	case 4:System.out.println("today is thursday");break;
+	case 5:System.out.println("today is friday");break;
+	case 6:System.out.println("today is saturday");break;
+	case 7:System.out.println("today is sunday");break;
+	default:System.out.println("your week day is invalid");
+	
+	
+	
+	}
+		
+		
+		
+		
+		
+
+	}
+
+}
